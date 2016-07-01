3d0a12737925278ede7b344efd0c3314f47bd0e8
Robert Stone <wobblybob@vm-deb-01.wobblybobz.local>
2016-07-01 09:43:11 +0800

    removed fixed jqueryui version from config

--- config-default.php
+++ config-default.php
@@ -2,7 +2,7 @@
 $config['url_prefix']='?page=';
 $config['url_postfix']='';
 
-$config['js']['versions']['jqueryui']='1.11.master';
+//$config['js']['versions']['jqueryui']='1.11.master';
 
 $config['dsn']='mysql://root:root@localhost/dam';
 
